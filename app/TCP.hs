79ec0bf4227109943366b7d6300d5bea659562b5
Chad Scherrer <chad.scherrer@gmail.com>
2017-04-25 05:10:50 -0700

    format as csv

--- app/TCP.hs
+++ app/TCP.hs
@@ -24,9 +24,10 @@ main = do
   -- printf "length words  == %d\n" (V.length words)
   -- printf "length docs   == %d\n" (V.length docs)
   -- printf "length topics == %d\n" (V.length topics)
+  putStrLn "true, pred"
   forM_ [0..(V.length topics - 1)] $ \i -> do
     --print $ V.map logFromLogFloat $ predict i
-    printf "%d %d\n" (topics ! i) (V.maxIndex $ predict i)
+    printf "%d, %d\n" (topics ! i) (V.maxIndex $ predict i)
   -- print $ accuracy
   --           topics
   --           (V.map (V.maxIndex . predict) (V.generate (V.length topics) id))
