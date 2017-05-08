32091519b604f39c0cd349e5013a92c4db82c109
Chad Scherrer <chad.scherrer@gmail.com>
2017-05-08 09:21:11 -0700

    make getNews output vocab

--- app/GetNews.hs
+++ app/GetNews.hs
@@ -1,25 +1,25 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as B
-import News (getNews)
+import News
 import qualified System.Random.MWC as MWC
 import Utils
 import qualified Data.Vector.Unboxed as V
 import Data.Vector.Unboxed ((!))
 import Text.Printf (printf)
-import NaiveBayes (prog)
 import Control.Monad (forever, replicateM, forM_)
 import Data.List (sort)
 import Data.Number.LogFloat
 import System.IO
-
 
 writeVec :: String -> V.Vector Int -> IO ()
 writeVec file v = withFile file WriteMode $ \h -> do
   V.forM_ v $ \x -> hPrint h x
 
 main = do
-  (words, docs, topics) <- getNews Nothing [0..]
+  ((words, docs, topics), enc) <- getNews Nothing [0..]
   writeVec "words" words
   writeVec "docs" docs
   writeVec "topics" topics
+  withFile "vocab" WriteMode $ \h -> do
+  	forM_ (vocab enc) $ \x -> B.hPutStrLn h x
