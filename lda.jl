67011a0ffc9a11fc2982e5b3f3896f378d5133bc
Chad Scherrer <chad.scherrer@gmail.com>
2017-06-06 12:41:20 -0700

    Exploratory Julia edits

--- lda.jl
+++ lda.jl
@@ -1,15 +1,52 @@
 using TopicModels
+using Iterators
+using StatsBase
+
+
+# function readDocs(docStream, wordStream)
+#   wordLines = readlines(wordStream)
+#   docCounts = counts([parse(x) for x in readlines(docStream)])
+#   dws =
+#   [map(dw -> dw[2], Iterators.groupby(dw -> dw[1], zip(docLines, wordLines)))
+# end
 
 docs = readDocuments(open("ldac"))
-lex = Array{String}(readLexicon(open("vocab")))
-numTopics = 5
-model = Model(fill(1.0, numTopics), 1.0, length(lex), Corpus(docs))
-trainModel(model, 2)
-# topWords = topTopicWords(model, lex, 7)
 
-for z in topTopicWords(model, lex, 7)
-  for w in z
+lex = Array{String}(readLexicon(open("vocab")))
+numTopics = 2
+
+function init()
+  m = Model(fill(1.0, numTopics), 1.0, length(lex), Corpus(deepcopy(docs)))
+  m.assignments = deepcopy(docs)
+  m.topics = [3 0; 0 3]
+  m.topicSums = [3.0, 3.0]
+  m.documentSums = [2 1; 1 2]
+  return m
+end
+
+m = init()
+
+n = 100000
+v = zeros(Int64, n)
+for i=1:n
+  m = init()
+  trainModel(m, 1)
+  v[i] = m.assignments[1][1]
+end
+
+for z in 1:numTopics
+  for w in topWords[z]
     println(w)
   end
-  println()
+  println("--------------")
+end
+
+trainModel(model,2)
+topWords = topTopicWords(model, lex, 7)
+
+for z in 1:numTopics
+  for w in topWords[z]
+    println(w)
+  end
+  println("--------------")
 end
--- summarize.jl
+++ summarize.jl
@@ -1,3 +1,5 @@
+Pkg.add("DataFrames")
+
 using DataFrames
 
 cd("/home/chad/git/iu/tcp/")
@@ -12,17 +14,32 @@ vocab = readcol("vocab")
 vocabcounts = counts(words, size(vocab)[1])
 
 vocab[sortperm(vocabcounts,rev=true)]
-sortperm(wordcounts,rev=true)
 samples = readtable("tcp2-gibbs.csv",header=false)
 z = Matrix(samples)[end,:]+1
+nTopics = maximum(z)
 
 df = DataFrame(d=docs, w=words,z=z)
 
-by(df, :z) do byz
+top = by(df, :z) do byz
   zvocabcounts = counts(byz[:w],size(vocab)[1])
-  ord = sortperm(zvocabcounts ÷ vocabcounts,rev=true)
+  ord = sortperm(zvocabcounts,rev=true)
   v = vocab[ord]
   DataFrame(v1=v[1],v2=v[2],v3=v[3],v4=v[4],v5=v[5],v6=v[6],v7=v[7])
 end
 
+for k = 1:nTopics
+  println("TOPIC $k")
+  for n = 1:7
+    println(top[k,parse("v$n")])
+  end
+  println()
+end
+
+vocab[words][100:120]
+
+vocabcounts[findfirst(vocab, "seconds")]
+vocab[sortperm(vocabcounts,rev=true)]
+
+top[1,:v4]
+parse("v$t")
 typeof(z)
